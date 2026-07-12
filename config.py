55d722407bd90ba80879bc986f52ad61b07b38bc
Andre Nicholas Jr <pumpgaming2@gmail.com>
2026-07-12 12:18:52 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+# =========================
+# MIDI TO PSYCH CONFIG
+# =========================
+
+SECTION_BEATS = 4
+START_CUTOFF_MS = 250
+REBASE_TO_ZERO = True
+
+ENGINE_VERSION = "Psych Engine 1.0.4"
+VERSION = "v0.0.1"
